70c5b9760cc67b8c46c207ccc1b80abfc1982043
BarakBarzilay <barakoyb@gmail.com>
2022-11-14 17:12:13 +0200

    finish css for nested ul elements

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import data from './data.json' assert { type: 'json' };
-let ulChildrenCount = 0;
+
+let depth = 0;
+let highestDepth = 0;
 
 document.querySelector('.app').innerHTML = `
     ${createList(data)}
 `;
 
-function createList(data) {
-  const items = data.map(createElement).join('');
+function createList(data, removeDepth) {
+  const items = data.map((i) => createElement(i)).join('');
+  if (removeDepth) {
+    depth--;
+  }
   return `${items}`;
 }
 
@@ -14,32 +19,63 @@ function createElement(item) {
   const { id, name, url, subData = null } = item;
   let subDataItem = [];
 
+  // check if need to create another list
   if (subData) {
-    ulChildrenCount++;
-    subDataItem = `
-    <li>
-      <ul class="ul-no-style" style='background-color: ${colorRenderer()}'>
-        ${createList(subData)}
-      </ul>
-    <li>
-    `;
+    subDataItem = createSubDataList(subData);
   }
-
-  function colorRenderer(arg) {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
-  }
-
+  console.log(depth);
   return `
+    <li>
+      <ul>
+      <li>Id:${id}</li>
+      <li>Site Name:${name}</li>
       <li>
-        <ul style='background-color: ${colorRenderer()}'>
-          <li>Id:${id}</li>
-          <li>Site Name:${name}</li>
-          <li>
-            <a href='${url}' target=”_blank”>Site Url:${name}</a>
-          </li>
-          ${subDataItem}
-        </ul>
+        <a href=https://${url} target=_blank>Site Url:${name}</a>
       </li>
-      
+     ${subDataItem}
+      </ul>
+    </li>
     `;
 }
+
+function createSubDataList(subData) {
+  depth++;
+  if (highestDepth < depth) highestDepth++;
+  return `
+      <ul class="ul-no-style">
+        ${createList(subData, true)}
+      </ul>`;
+}
+
+function randomInteger(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function randomRgbColor() {
+  let r = randomInteger(255);
+  let g = randomInteger(255);
+  let b = randomInteger(255);
+  return [r, g, b];
+}
+
+// adding css style according to depth of nested ul elements
+function addStyleToBody() {
+  // adding first depth count
+  highestDepth++;
+  //create the style element and appending it
+  const head = document.head || document.getElementsByTagName('head')[0];
+  const style = document.createElement('style');
+  head.appendChild(style);
+
+  const twoUlElements = ' ul ul';
+  // for every step into depth of ul inside node, add ul ul selectors
+  for (let i = 0; i < 16; i++) {
+    style.innerHTML += `
+        ul ul${twoUlElements.repeat(i)} {
+          background-color: rgb(${randomRgbColor()})
+        }
+      `;
+  }
+}
+console.log(highestDepth);
+addStyleToBody();
